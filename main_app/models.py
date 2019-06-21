75e6e0a409a8b2e8a368520820f3d2c2170df9c0
Chris Williams <cwill833@gmail.com>
2019-06-21 15:43:50 -0500

    assessment done

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Items(models.Model):
+    description = models.TextField(max_length=250)
+    
+    def __str__(self):
+        return self.description
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Items
 
-# Create your views here.
+
+
+# Define the home view
+def home(request):
+    items = Items.objects.all()
+    return render(request, 'home.html', {
+      'items': items
+    })
+
+class ItemCreate(CreateView):
+    model = Items
+    fields = "__all__"
+    success_url = '/'
+
+class ItemDelete(DeleteView):
+    model = Items
+    success_url = '/'
